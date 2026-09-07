b1aa316aee41cc6b58c441e0809b74a904f69675
Putra Alana P <putraalanaprasetya@gmail.com>
2026-09-07 19:53:16 +0700

    done
    Enable error reporting for debugging during login process.

--- proses_login.php
+++ proses_login.php
@@ -1,6 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require 'koneksi.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
