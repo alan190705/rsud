ef42525a161d06ca703beef7bb11196b40cd4ca5
Putra Alana P <putraalanaprasetya@gmail.com>
2026-09-07 19:32:38 +0700

    Exclude sensitive files

--- hash_generator.php
+++
@@ -1,6 +0,0 @@
-<?php
-$plain_password = "admin"; // Ganti dengan password yang ingin Anda gunakan
-$hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
-echo "Password asli: " . $plain_password . "<br>";
-echo "Hash password: " . $hashed_password;
-?>
--- test_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$password = 'admin123';
-
-$hash = password_hash($password, PASSWORD_DEFAULT);
-
-echo "Password: " . $password . "<br><br>";
-echo "Hash baru:<br>";
-echo "<textarea style='width:600px;height:100px;'>" . htmlspecialchars($hash) . "</textarea>";
